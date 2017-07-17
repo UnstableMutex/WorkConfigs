12ccc30cf2241f3839269683fe8c3c300f29d523
Dmitry <stormbringer-s@mail.ru>
2017-07-17 16:07:13 +0400

    up

--- prodmia.ps1
+++ prodmia.ps1
@@ -1,2 +1,2 @@
 Copy-Item -Path C:\Users\storm\Desktop\Github\WorkConfigs\ConfigOnProdMia\Releases -Destination C:\Users\storm\Desktop\SVN\focus -recurse -Force
-Copy-Item -Path C:\Users\storm\Desktop\SVN\focus\Releases\prodmia\bin\Data -Destination C:\Users\storm\Desktop\SVN\focus\Releases\1.3.3\bin\Data -recurse -Force
+Copy-Item -Path C:\Users\storm\Desktop\SVN\focus\Releases\prodmia\bin\Data -Destination C:\Users\storm\Desktop\SVN\focus\Releases\1.3.3\bin -recurse -Force
